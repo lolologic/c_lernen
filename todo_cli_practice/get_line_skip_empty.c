42860a1376240dba5464f721f6a3d30d04ae0649
lolologic <227577287+lolologic@users.noreply.github.com>
2026-08-26 20:30:57 +0200

    wip: beginne EOF- und Leerzeilenbehandlung in get_line
    - erweitere get_line um mehrere Zeilenaufrufe
    - beginne Behandlung von EOF und leeren Zeilen
    - teste Rückgabe mehrerer dynamisch gelesener Zeilen
    - Zwischenstand noch nicht vollständig funktionsfähig

---
+++ todo_cli_practice/get_line_skip_empty.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *get_line(FILE *f);
+
+int main(void) {
+
+    FILE *f = fopen("test.txt", "r");
+
+    if (f == NULL) {
+
+        printf("Dateifehler.\n");
+        return 1;
+    }
+
+    char *line1 = get_line(f);
+    char *line2 = get_line(f);
+    char *line3 = get_line(f);
+
+    if (line1 == NULL) {
+
+        printf("Speicherfehler.\n");
+        fclose(f);
+        return 1;
+
+    } else if (line2 == NULL) {
+
+        printf("Speicherfehler.\n");
+        fclose(f);
+        return 1;
+
+    } else if (line3 == NULL) {
+
+        printf("Speicherfehler.\n");
+        fclose(f);
+        return 1;
+    }
+
+    printf("Erste Zeile: %s\n", line1);
+    printf("Zweite Zeile: %s\n", line2);
+    printf("Dritte Zeile: %s\n", line3);
+
+    free(line1);
+    free(line2);
+    free(line3);
+    line1 = NULL;
+    line2 = NULL;
+    line3 = NULL;
+    fclose(f);
+
+    return 0;
+}
+
+char *get_line(FILE *f) {
+
+    int capacity = 4;
+    int count = 0;
+
+    char *text = malloc(capacity * sizeof(char));
+
+    if (text == NULL) {
+
+        return NULL;
+    }
+
+    int c;
+
+    while ((c = fgetc(f)) != EOF) {
+
+        count = 0;
+
+        while (c != '\n') {
+
+            if (count > 0) {
+
+                int newCapacity = capacity * 2;
+                char *temp = realloc(text, newCapacity * sizeof(char));
+
+                if (temp == NULL) {
+
+                    printf("Speicherfehler.\n");
+                    free(text);
+                    return NULL;
+                }
+
+                text = temp;
+                capacity = newCapacity;
+
+            } else {
+                
+                break;
+            }
+
+            text[count] = c;
+            count++;
+        }
+    }
+
+
+    text[count] = '\0';
+
+    printf("%d\n", count);
+
+    return text;
+}
